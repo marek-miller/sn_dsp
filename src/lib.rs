b99aeeade64a6fbb7c6de4b8e4a9057ef8b7e999
Marek Miller <mlm@math.ku.dk>
2023-08-14 13:28:54 +0200

    Add Control/Reset trait

--- src/lib.rs
+++ src/lib.rs
@@ -24,3 +24,28 @@ pub mod prelude;
 pub fn alloc_buffer<T: Default>(size: usize) -> Box<[T]> {
     (0..size).map(|_| T::default()).collect()
 }
+
+pub trait Reset {
+    fn reset(&mut self);
+}
+
+pub trait Control {
+    type Ctl<'a>
+    where
+        Self: 'a;
+
+    fn control(
+        &mut self,
+        f: impl FnOnce(&mut Self::Ctl<'_>),
+    );
+}
+
+impl<T> Reset for T
+where
+    T: Control,
+    for<'a> <T as Control>::Ctl<'a>: Reset,
+{
+    fn reset(&mut self) {
+        self.control(|ctl| ctl.reset());
+    }
+}
---
+++ tests/ctl_reset.rs
@@ -0,0 +1,99 @@
+use sn_dsp::{
+    prelude::Fp,
+    Control,
+    Reset,
+};
+
+struct Mock {
+    feedback: Fp,
+}
+
+struct MockCtl<'a> {
+    a: &'a mut Fp,
+}
+
+impl<'a> Reset for MockCtl<'a> {
+    fn reset(&mut self) {
+        *self.a = 77.;
+    }
+}
+
+impl Control for Mock {
+    type Ctl<'a> = MockCtl<'a> where Self: 'a;
+
+    fn control(
+        &mut self,
+        f: impl FnOnce(&mut Self::Ctl<'_>),
+    ) {
+        let mut ctl = MockCtl {
+            a: &mut self.feedback,
+        };
+        f(&mut ctl);
+        self.feedback = *ctl.a + 1.;
+    }
+}
+
+#[test]
+fn check_control_01() {
+    let mut mock = Mock {
+        feedback: 9.
+    };
+    assert_eq!(mock.feedback, 9.);
+
+    mock.control(|fbk| *fbk.a = 11.);
+
+    assert_eq!(mock.feedback, 12.)
+}
+
+#[test]
+fn check_reset_01() {
+    let mut mock = Mock {
+        feedback: 9.
+    };
+    assert_eq!(mock.feedback, 9.);
+
+    mock.control(|fbk| fbk.reset());
+
+    assert_eq!(mock.feedback, 78.)
+}
+
+#[test]
+fn check_reset_impl() {
+    let mut mock = Mock {
+        feedback: 9.
+    };
+    assert_eq!(mock.feedback, 9.);
+
+    mock.reset();
+
+    assert_eq!(mock.feedback, 78.)
+}
+
+struct Mock2 {
+    feedback: Fp,
+}
+
+impl Control for Mock2 {
+    type Ctl<'a> = (&'a mut Fp,);
+
+    fn control(
+        &mut self,
+        f: impl FnOnce(&mut Self::Ctl<'_>),
+    ) {
+        f(&mut (&mut self.feedback,));
+    }
+}
+
+#[test]
+fn check_control_mock2_01() {
+    let mock2 = Mock2 {
+        feedback: 9.
+    };
+    assert_eq!(mock2.feedback, 9.);
+
+    let mut m = mock2;
+    m.control(|fbk| *fbk.0 = 11.);
+
+    let mock2 = m;
+    assert_eq!(mock2.feedback, 11.);
+}
